d8fe8637de149275d786cf7805185463ec04346a
prawish07 <prawish.lamichhane20@gmail.com>
2026-08-12 20:51:25 +0545

    Add html and script files

---
+++ Day 3/tl.js
@@ -0,0 +1,6 @@
+//`template literals`--variables and expressions inside a string.
+let firstName="Prabesh"
+let lastName="Lamichhane"
+// let fullName=firstName+lastName
+let fullName=`${firstName} ${lastName}`
+console.log(fullName)
